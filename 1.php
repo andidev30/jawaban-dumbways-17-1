d3b65d273813b281a467c7aeb8b2bd4cb226c438
andi <aandi30082001@gmail.com>
2020-06-13 16:52:39 +0700

    resolve 1.php

--- 1.php
+++ 1.php
@@ -1,34 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>1</title>
-</head>
+<?php 
 
-<body>
-    
-    <a href="1.php">Refresh</a>
-    <br>
-    <form action="" method="get">
-        <label>Masukan angka binner</label>
-        <input type="text" name="angka">
-        <button type="submit" name="btn">cek</button>
-    </form>
-    <br>
-
-    <?php 
-        if(isset($_GET['btn'])){
-            $angka = $_GET['angka'];
-            if(is_numeric($angka)){
-                $bin = $angka;
-                $hex2 = base_convert($bin,2,16);
-                echo 'result : ' . $hex2;
-            } else {
-                echo "inputan harus angka";
-            }
+    function cetak($param){
+        if(is_numeric($param)){
+            $bin = $param;
+            $hex2 = base_convert($bin,2,16);
+            $result = $hex2;
+            return $result;
+        } else {
+            return $result ="inputan harus angka";
         }
-    ?>
+    }
 
-</body>
-</html>
+    echo cetak(1110);
+    
+?>
