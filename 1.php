5ef56f673afa7b8048bf371df4e0a766207a1e60
andi <aandi30082001@gmail.com>
2020-06-13 15:46:53 +0700

    solve 1.php

--- 1.php
+++ 1.php
@@ -5,10 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>1</title>
 </head>
+
 <body>
     
-    <a href="refresh">Refresh</a>
-    <form action="" method="get"></form>
+    <a href="1.php">Refresh</a>
+    <br>
+    <form action="" method="get">
+        <label>Masukan angka binner</label>
+        <input type="text" name="angka">
+        <button type="submit" name="btn">cek</button>
+    </form>
+    <br>
+
+    <?php 
+        if(isset($_GET['btn'])){
+            $angka = $_GET['angka'];
+            if(is_numeric($angka)){
+                $bin = $angka;
+                $hex2 = base_convert($bin,2,16);
+                echo 'result : ' . $hex2;
+            } else {
+                echo "inputan harus angka";
+            }
+        }
+    ?>
 
 </body>
 </html>
